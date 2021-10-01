fb7ff7be6653760e346275bf3c72224934e2f929
JuraKre <juergen.kreuter@duplexmedia.com>
2021-10-01 23:07:44 +0200

    :sparkles: introducing new frontend framework

--- configuration/webpack.prod.config.js
+++ configuration/webpack.prod.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
