2873c69f9c2b7efe8531da70f0ca798e23133253
JuraKre <juergen.kreuter@duplexmedia.com>
2021-10-01 23:00:24 +0200

    :sparkles: introducing new frontend framework

--- configuration/webpack.prod.config.js
+++ configuration/webpack.prod.config.js
@@ -8,10 +8,8 @@ const webpackConfiguration = require('../webpack.config');
 module.exports = merge(webpackConfiguration, {
   mode: 'production',
 
-  /* Manage source maps generation process. Refer to https://webpack.js.org/configuration/devtool/#production */
   devtool: false,
 
-  /* Optimization configuration */
   optimization: {
     minimize: true,
     minimizer: [
@@ -22,12 +20,10 @@ module.exports = merge(webpackConfiguration, {
     ],
   },
 
-  /* Performance treshold configuration values */
   performance: {
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
   },
 
-  /* Additional plugins configuration */
   plugins: [],
 });
